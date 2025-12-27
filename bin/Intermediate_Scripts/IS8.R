f449d9dbf8212daa3624cdf42290899f4f6bc797
Praveena Naidu <praveena@MacBook-Pro.lan>
2025-12-27 12:22:50 -0500

    is8 complete

--- bin/Intermediate_Scripts/IS8.R
+++ bin/Intermediate_Scripts/IS8.R
@@ -37,9 +37,9 @@ transdf_massspec <- left_join(transdf,mass_spec, by = "Transdecoder_ID")
 
 
 #for the final R app for species with no genome but with mass spec data.gz to reduce file size
-write.csv(transdf_massspec, file=gzfile(paste0(Sample_name, "_distinct_masspec.csv.gz")), row.names = FALSE)
+write.csv(transdf_massspec, file=gzfile(paste0(Sample_name, "_transdf_distinct_masspec.csv.gz")), row.names = FALSE)
 
-
+write.csv(transdf_massspec, paste0(Sample_name, "_transdf_distinct_masspec.csv"), row.names = FALSE)
 filtered_mass_spec <- left_join(transdf_filtered,mass_spec, by = "Transdecoder_ID")
 filtered_select <- filtered_mass_spec[, c(1:32, 36)]
 write.csv(filtered_select, paste0(Sample_name, "_filtered_masspec_select.csv"), row.names = FALSE)
