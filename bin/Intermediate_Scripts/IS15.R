e0f0f47e95a772d4229598b7fa6c060d32c0e03b
lexatrash <prel97@gmail.com>
2026-05-07 15:31:22 -0400

    fill colours replace

--- bin/Intermediate_Scripts/IS15.R
+++ bin/Intermediate_Scripts/IS15.R
@@ -155,11 +155,13 @@ filter_and_venn <- function(Base, pattern, pattern2, mass = FALSE, strict = TRUE
   
   union_ids <- Reduce(union, sets)
   Base_union <- Base[Base$Transdecoder_ID %in% union_ids, ] %>% mutate(Filter = if(strict) "Strict" else "Lax")
-  return(Base_union)
+  
   
   Pepseq <- AAStringSet(Base_union$PEP_Sequence)
   names(Pepseq) <- Base_union$Transdecoder_ID
   writeAAStringSet(Pepseq, paste0(Sample_name,"_filtered_",file_suffix,".pep"))
+  
+  return(Base_union)
 
 }
 
